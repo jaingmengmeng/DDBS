03baf0676f6f38859082625d16dedd688bc884cf
jaingmengmeng <1804226997@qq.com>
2020-11-28 12:42:06 +0800

    将数据分到不同site上

--- data-loader/DataLoader.cpp
+++ data-loader/DataLoader.cpp
@@ -4,7 +4,7 @@ DataLoader::DataLoader() {
     // initialize file_path;
     this->files[std::string("Book")] = std::string("../files/data/book.tsv");
     this->files[std::string("Customer")] = std::string("../files/data/customer.tsv");
-    this->files[std::string("Orders")] = std::string("../files/data/order.tsv");
+    this->files[std::string("Orders")] = std::string("../files/data/orders.tsv");
     this->files[std::string("Publisher")] = std::string("../files/data/publisher.tsv");
     this->sites.push_back(std::string("site1"));
     this->sites.push_back(std::string("site2"));
@@ -12,10 +12,10 @@ DataLoader::DataLoader() {
     this->sites.push_back(std::string("site4"));
 
     // create Tables;
-    Relation* Book = new Relation(std::string("Book"));
-    Relation* Customer = new Relation(std::string("Customer"));
-    Relation* Orders = new Relation(std::string("Orders"));
-    Relation* Publisher = new Relation(std::string("Publisher"));
+    Relation* Book = new Relation(std::string("Book"), true);
+    Relation* Customer = new Relation(std::string("Customer"), false);
+    Relation* Orders = new Relation(std::string("Orders"), true);
+    Relation* Publisher = new Relation(std::string("Publisher"), true);
     this->relations.push_back(Book);
     this->relations.push_back(Customer);
     this->relations.push_back(Orders);
@@ -129,7 +129,9 @@ std::vector<std::string> tsv_split(const std::string& s, const std::string& c) {
 }
 
 std::string Convert2Insert(Relation* r, std::vector<std::vector<std::string> > v) {
-    std::string res = std::string("INSERT INTO ");
+    std::string res = std::string("");
+    if(v.size() == 0) return res;
+    res += std::string("INSERT INTO ");
     res += r->rname;
     res += std::string("(");
     for(int i=0; i<r->attributes.size(); ++i) {
@@ -162,6 +164,76 @@ std::string Convert2Insert(Relation* r, std::vector<std::vector<std::string> > v
     return res;
 }
 
+std::map<std::string, std::vector<std::vector<std::string>>> DataLoader::data_fragment(Relation* relation, std::vector<std::vector<std::string>> datas) {
+    std::map<std::string, std::vector<std::vector<std::string>>> allocated_data_map;
+    if(relation->is_horizontal) {
+        for(auto data : datas) {
+            for(auto fragment : relation->frags) {
+                std::vector<bool> result;
+                for(auto predicate : fragment.hf_condition) {
+                    for(int i=0; i<relation->attributes.size(); ++i) {
+                        if(relation->attributes[i].aname == predicate.aname) {
+                            switch (predicate.op_type) {
+                                case 1:
+                                    if(atof(data[i].c_str()) >= predicate.num) {
+                                        result.push_back(true);
+                                    }
+                                    break;
+                                case 2:
+                                    if(atof(data[i].c_str()) <= predicate.num) {
+                                        result.push_back(true);
+                                    }
+                                    break;
+                                case 3:
+                                    if(atof(data[i].c_str()) > predicate.num) {
+                                        result.push_back(true);
+                                    }
+                                    break;
+                                case 4:
+                                    if(atof(data[i].c_str()) < predicate.num) {
+                                        result.push_back(true);
+                                    }
+                                    break;
+                                case 5:
+                                    if(atof(data[i].c_str()) == predicate.num) {
+                                        result.push_back(true);
+                                    }
+                                    break;
+                                case 6:
+                                    if(data[i] == predicate.str) {
+                                        result.push_back(true);
+                                    }
+                                    break;
+                                case 7:
+                                    break; 
+                                default:
+                                    break;
+                            }
+                        }
+                    }
+                }
+                if(result.size() == fragment.hf_condition.size())
+                    allocated_data_map[fragment.sname].push_back(data);
+            }
+        }
+    } else {
+        for(auto data : datas) {
+            for(auto fragment : relation->frags) {
+                std::vector<std::string> tmp;
+                for(auto aname : fragment.vf_condition) {
+                    for(int i=0; i<relation->attributes.size(); ++i) {
+                        if(relation->attributes[i].aname == aname) {
+                            tmp.push_back(data[i]);
+                        }
+                    }
+                }
+                allocated_data_map[fragment.sname].push_back(tmp);
+            }
+        }
+    }
+    return allocated_data_map;
+}
+
 void DataLoader::load_data() {
     std::string file_error = std::string("Error opening file. Please check your filename.");
     for(auto relation : this->relations) {
@@ -169,13 +241,24 @@ void DataLoader::load_data() {
         std::ifstream fin(file_path, std::ios_base::in);
         std::string str;
         if (fin.is_open()) {
-            std::vector<std::vector<std::string> > vv_str;
+            std::vector<std::vector<std::string>> vv_str;
             while(std::getline(fin, str)) {
+                // 根据制表符分割字符串
                 std::vector<std::string> v_str = tsv_split(str, std::string("\t"));
                 vv_str.push_back(v_str);
             }
-            std::string insert_str = Convert2Insert(relation, vv_str);
-            // std::cout << insert_str << std::endl;
+            // 根据分片信息将所有数据进行分到不同的site上
+            std::map<std::string, std::vector<std::vector<std::string>>> allocated_data_map = this->data_fragment(relation, vv_str);
+            std::vector<std::string> insert_str;
+            for(auto sname : this->sites) {
+                std::string insert = Convert2Insert(relation, allocated_data_map[sname]);
+                if(insert != std::string("")) {
+                    insert_str.push_back(insert);
+                    std::ofstream fout(sname+"_data_loader.sql", std::ios::app);
+                    fout << insert << std::endl;
+                    fout.close();
+                }
+            }
         } else {
             std::cout << file_error << std::endl;
             exit(1);
