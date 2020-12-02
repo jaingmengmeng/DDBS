f17e59f1bdf337ed68dc61580ed825a900bd76ac
swh <2521285258@qq.com>
2020-12-02 10:01:10 +0800

    simple on-server  dataloading test

--- network-utils/network-test.cpp
+++ network-utils/network-test.cpp
@@ -8,14 +8,24 @@ int main(int argc, char* argv[]){
 //    std::cout << read_from_etcd_by_key("/test-key1");
 
     std::string host = "127.0.0.1:8000";
-    std::string table_name = "test2";
+    std::string table_name = "test";
     std::string attr_meta = "`id` int(11) not null, `name` varchar(20) not null";
     std::vector<std::string> attr_values;
     attr_values.emplace_back("1, 'a'");
     attr_values.emplace_back("2, 'b'");
     attr_values.emplace_back("3, 'c'");
 
-    return load_table(host, table_name, attr_meta, attr_values);
+    std::vector<std::string> hosts;
+    hosts.emplace_back("10.77.70.172:8000");
+    hosts.emplace_back("10.77.70.188:8000");
+    hosts.emplace_back("10.77.70.189:8000");
+    for (int i = 0; i < 3; i++)
+    {
+        load_table(hosts[i], table_name + std::to_string(i), attr_meta, attr_values);
+    }
+    
+
+    // return load_table(host, table_name, attr_meta, attr_values);
 
     return 0;
 }
